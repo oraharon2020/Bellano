28ba4be5cf0b7f98112a5dbfabe7b759af5d7258
Or Aharon <or.aharon1122@gmail.com>
2025-12-24 21:30:26 +0200

    שינוי טקסט: הרכבה עצמאית / דרכינו

--- src/app/product/[slug]/ProductPageClient.tsx
+++ src/app/product/[slug]/ProductPageClient.tsx
@@ -765,7 +765,7 @@ export function ProductPageClient({ product, variations = [], faqs = [], video =
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <span>{product.assemblyIncluded !== false ? 'מגיע מורכב' : 'דרושה הרכבה עצמית'}</span>
+                <span>{product.assemblyIncluded !== false ? 'מגיע מורכב' : 'הרכבה עצמאית / דרכינו'}</span>
               </div>
             </div>
 
